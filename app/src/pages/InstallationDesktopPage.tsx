6dea5ee23a078950a418e1f4bf713978b7386b44
Lara Amadeo <laraamadeo@Toqios-MacBook-Pro.local>
2023-09-16 14:20:55 +0200

    add qr redirect to yuper deployment

--- app/src/pages/InstallationDesktopPage.tsx
+++ app/src/pages/InstallationDesktopPage.tsx
@@ -5,7 +5,7 @@ import ReactCardFlip from 'react-card-flip';
 
 export default function InstallationDesktopPage(): JSX.Element {
     const motto = 'Sharing meals, saving time.Where food creates community!'
-    const clickMe = 'Click me to scan QR and start enjoying'
+    const clickMe = 'Click me to scan QR Start enjoying!'
 
     const [flipped, setFlipped] = useState(false)
 
@@ -23,20 +23,16 @@ export default function InstallationDesktopPage(): JSX.Element {
                     <div className='insP-app-icon-container'>
                         <ReactCardFlip isFlipped={flipped}>
                             <div onClick={() => setFlipped(!flipped)}>
-                                <img src='/apple-touch-icon-180x180.png' className='insP-icon'></img>
+                                <img src='/other/logo-flip.svg' className='insP-icon'></img>
                             </div>
 
                             <div onClick={() => setFlipped(!flipped)}>
-                                <img src='/maskable-icon-512x512.png' className='insP-icon'></img>
+                                <img src='/other/qr-flip.svg' className='insP-icon'></img>
                             </div>
                         </ReactCardFlip>
                     </div>
 
-                    <div className='insP-arrow'>
-                        <img src='/other/arrow.png' className='insP-arrow'></img>
-                    </div>
-
-                    <p className='body.text grey-700 insP-motto'>{clickMe}</p>
+                    <p className='body.text grey-700 insP-motto'>Click me to scan QR <strong>Start enjoying!</strong></p>
 
                 </div>
 
